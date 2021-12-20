53a058e6dd312d76588822ac03a09312a8884191
Mikha1lSorokin <sorokin.mishaaa@yandex.ru>
2021-12-20 20:24:45 +0300

    add lab4

---
+++ Lab4.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "mpi.h"
+#include <ctime>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	MPI_Init(&argc, &argv);
+	int rank, n, i, message;
+	MPI_Status status;
+	MPI_Comm_size(MPI_COMM_WORLD, &n);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	
+	srand(time(NULL));
+	int chosen_thread = (rand() % 7) + 1;
+
+	if (rank == 0)
+	{
+		MPI_Recv(&message, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+		cout << "Hello! It's process number " << message << "!\n";
+		MPI_Recv(&message, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+		cout << "My random nonzero integer is: " << message << "\n";
+	}
+
+	else if (rank == chosen_thread)
+	{
+		srand(time(NULL) + rank);
+		int nonzero_number = (rand() % 999);
+		MPI_Send(&rank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+		MPI_Send(&nonzero_number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	}
+
+	else 	
+	{
+		int zero_number = 0;
+	}
+
+	MPI_Finalize();
+	return 0;
+}
